c1578c950467315e97a217b25f2af4b4683d96c1
Gautam J <gautam.jayapal@gmail.com>
2022-01-23 19:58:35 +0530

    update home add basic

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,22 +1,22 @@
 import { FunctionComponent } from "react";
-import { BsArrowDown } from "react-icons/bs";
+import { BsArrowDown, BsFillArrowUpCircleFill } from "react-icons/bs";
 import { IRole } from "../utils/types";
+import homeLight from "../public/assets/homeLight.svg";
+import Image from "next/image";
 
 const Intro: FunctionComponent<{
   data: IRole[];
 }> = ({ data }) => {
   return (
     // h-screen can be used
-    <div className="grid items-center grid-cols-12 introContainer">
-      <h1 className="col-span-8 col-start-2 mt-12 font-extrabold text-8xl">
-        $ home
-      </h1>
+    <div className="relative grid items-center grid-cols-12 introContainer">
+      <h1 className="col-span-8 col-start-2 mt-12 linux">$ home</h1>
 
       <div className="col-span-6 col-start-3 mb-24">
-        <h1 className="font-bold text-7xl">Hi, I&apos;m Gautam!</h1>
+        <h1 className="mainHeading">Hi, I&apos;m Gautam!</h1>
 
         {/* TODO: add typing carousel animation */}
-        <div className="col-start-2 col-span-full">
+        <div>
           {data.map((item) => (
             <p
               key={item._id}
@@ -28,9 +28,17 @@ const Intro: FunctionComponent<{
         </div>
       </div>
 
+      <div className="absolute justify-center col-start-9 col-span-full">
+        <Image src={homeLight} alt="Home Graphic" className="scale-75" />
+      </div>
+
       <div className="flex items-center justify-center col-start-1 col-span-full">
         <BsArrowDown className="text-7xl" />
       </div>
+
+      <a href="">
+        <BsFillArrowUpCircleFill className="fixed text-5xl bottom-14 right-14" />
+      </a>
     </div>
   );
 };
