ad90e8885e8c40b3da7734bc1e5a24dca8d9bd0d
Gautam J <gautam.jayapal@gmail.com>
2022-01-26 20:59:00 +0530

    update project component

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent } from "react";
 import { IProject } from "../utils/types";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import projectLight from "../public/assets/projectLight.svg";
+import projectDark from "../public/assets/projectDark.svg";
 
 export const Project: FunctionComponent<{ data: IProject[] }> = ({ data }) => {
   if (!process.env.S3_BUCKET_NAME) {
@@ -11,21 +14,58 @@ export const Project: FunctionComponent<{ data: IProject[] }> = ({ data }) => {
   }
 
   const baseURL = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_BUCKET_REGION}.amazonaws.com`;
+  const { theme } = useTheme();
+
   return (
-    <div>
-      <h1>Project</h1>
-      <div>
+    <div className="relative grid items-center justify-center min-h-screen grid-cols-12 h-max bg-gradient-to-b from-light-base01 to-light-base00 dark:from-dark-base01 dark:to-dark-base00">
+      <div className="absolute top-0 right-0">
+        <Image
+          src={theme === "light" ? projectLight : projectDark}
+          alt="Projects Graphic"
+          width="350px"
+          height="350px"
+        />
+      </div>
+      <h1 className="z-10 col-start-8 mt-10 linux col-span-full">$ grep</h1>
+      <div className="col-start-2 col-span-full">
+        <h1 className="mt-10 mb-16 mainHeading">Projects</h1>
+      </div>
+
+      <div className="grid items-start grid-cols-3 col-start-2 col-end-12 mb-10 gap-x-20 gap-y-14">
         {data.map((project) => (
-          <div key={project.title}>
-            <h3>{project.title}</h3>
-            <p>{new Date(project.date).toDateString()}</p>
-            <p>{project.link}</p>
-            <Image
-              src={`${baseURL}/${project.imageURL}`}
-              alt={project.title}
-              width={400}
-              height={300}
-            />
+          <div
+            key={project.title}
+            className="overflow-hidden rounded-xl drop-shadow-2xl"
+          >
+            <div className="p-5 bg-gradient-to-b from-light-base01 to-light-base00 dark:from-dark-base01 dark:to-dark-base00">
+              <h3 className="mb-1 text-2xl font-bold text-light-base0D dark:text-dark-base0D">
+                {project.title}
+              </h3>
+              <span className="text-light-base09 dark:text-dark-base09">
+                {new Date(project.date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                })}
+                {"  "}|{" "}
+                <a
+                  className="underline"
+                  href={project.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </span>
+            </div>
+            <div className="w-full h-full">
+              <Image
+                src={`${baseURL}/${project.imageURL}`}
+                alt={project.title}
+                width={200}
+                height={150}
+                layout="responsive"
+              />
+            </div>
           </div>
         ))}
       </div>
