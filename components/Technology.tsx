ede1b6134243e5a0633379e056da975c00a95638
Gautam J <gautam.jayapal@gmail.com>
2022-01-27 23:20:19 +0530

    add technology animation

--- components/Technology.tsx
+++ components/Technology.tsx
@@ -30,57 +30,100 @@ const Technology: FunctionComponent<{ data: ITechnology[] }> = ({ data }) => {
       id="Technology"
       className="relative grid items-center justify-center min-h-screen grid-cols-12 h-max bg-gradient-to-b from-light-base00 to-light-base01 dark:from-dark-base00 dark:to-dark-base01"
     >
-      <h1 className="col-start-2 mt-4 lg:mt-14 linux col-span-full">
+      <motion.h1
+        variants={linux}
+        initial="initial"
+        whileInView="animate"
+        className="col-start-2 mt-4 lg:mt-14 linux col-span-full"
+      >
         $ groups
-      </h1>
+      </motion.h1>
       <div className="flex justify-center col-start-1 col-span-full lg:col-start-7 lg:col-span-full">
-        <h1 className="my-10 lg:my-12 mainHeading">Technology</h1>
+        <motion.h1
+          variants={leftToRight}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true }}
+          className="my-10 lg:my-12 mainHeading"
+        >
+          Technology
+        </motion.h1>
       </div>
 
       <div className="col-start-2 col-end-12 lg:col-start-2 lg:col-end-10">
         <div className="flex flex-col items-start justify-center mb-8">
-          <h3 className="px-3 text-3xl font-bold lg:px-6 lg:text-4xl text-light-base0D dark:text-dark-base0D">
+          <motion.h3
+            variants={leftToRight}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className="px-3 text-3xl font-bold lg:px-6 lg:text-4xl text-light-base0D dark:text-dark-base0D"
+          >
             Languages
-          </h3>
+          </motion.h3>
           <div className="flex flex-wrap items-center p-4 my-4 justify-evenly lg:justify-start lg:p-6 drop-shadow-xl gap-x-4 gap-y-8 rounded-3xl bg-gradient-to-r from-light-base01 to-light-base00 dark:from-dark-base00 dark:to-dark-base01">
             {languages.map((item) => (
-              <div key={item}>
+              <motion.div
+                variants={graphic}
+                initial="initial"
+                whileInView="animate"
+                viewport={{ once: true }}
+                key={item}
+              >
                 <Image
                   src={`${baseURL}/${item}.svg`}
                   alt={item}
                   width="75px"
                   height="75px"
                 />
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
       </div>
 
       <div className="col-start-2 col-end-12 lg:col-start-4 lg:col-end-12">
-        <div className="absolute hidden lg:block left-8">
+        <motion.div
+          variants={graphic}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true }}
+          className="absolute hidden lg:block left-8"
+        >
           <Image
             src={theme === "light" ? technologyLight : technologyDark}
             alt="Technology graphic"
             width={300}
             height={300}
           />
-        </div>
+        </motion.div>
 
         <div className="flex flex-col items-end justify-center mb-8">
-          <h3 className="px-3 text-3xl font-bold lg:px-6 lg:text-4xl text-light-base0D dark:text-dark-base0D">
+          <motion.h3
+            variants={leftToRight}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className="px-3 text-3xl font-bold lg:px-6 lg:text-4xl text-light-base0D dark:text-dark-base0D"
+          >
             Frameworks
-          </h3>
+          </motion.h3>
           <div className="flex flex-wrap items-center p-4 my-4 justify-evenly lg:justify-end lg:p-6 drop-shadow-xl gap-x-4 rounded-3xl gap-y-8 bg-gradient-to-r from-light-base00 to-light-base01 dark:from-dark-base01 dark:to-dark-base00">
             {frameworks.map((item) => (
-              <div key={item}>
+              <motion.div
+                variants={graphic}
+                initial="initial"
+                whileInView="animate"
+                viewport={{ once: true }}
+                key={item}
+              >
                 <Image
                   src={`${baseURL}/${item}.svg`}
                   alt={item}
                   width="75px"
                   height="75px"
                 />
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
@@ -88,31 +131,49 @@ const Technology: FunctionComponent<{ data: ITechnology[] }> = ({ data }) => {
 
       <div className="col-start-2 col-end-12 mb-6 lg:col-start-2 lg:col-end-10">
         <div className="flex flex-col items-start justify-center lg:mb-8">
-          <h3 className="px-3 text-3xl font-bold lg:px-6 lg:text-4xl text-light-base0D dark:text-dark-base0D">
+          <motion.h3
+            variants={leftToRight}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className="px-3 text-3xl font-bold lg:px-6 lg:text-4xl text-light-base0D dark:text-dark-base0D"
+          >
             Others
-          </h3>
+          </motion.h3>
           <div className="flex flex-wrap items-center p-4 my-4 justify-evenly lg:justify-start lg:p-6 drop-shadow-xl rounded-3xl gap-x-5 gap-y-8 bg-gradient-to-r from-light-base01 to-light-base00 dark:from-dark-base00 dark:to-dark-base01">
             {others.map((item) => (
-              <div key={item}>
+              <motion.div
+                variants={graphic}
+                initial="initial"
+                whileInView="animate"
+                viewport={{ once: true }}
+                key={item}
+              >
                 <Image
                   src={`${baseURL}/${item}.svg`}
                   alt={item}
                   width="75px"
                   height="75px"
                 />
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center col-start-2 col-end-12 mb-8 lg:hidden">
+      <motion.div
+        variants={graphic}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true }}
+        className="flex items-center justify-center col-start-2 col-end-12 mb-8 lg:hidden"
+      >
         <Image
           src={theme === "light" ? technologyLight : technologyDark}
           alt="Technology graphic"
           width={200}
           height={200}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
